75c47185ff52fd7ba7ab8af15e16903c31c2b3c7
acalinovic <boris.acalinovic@gmail.com>
2017-03-20 11:05:25 +0100

    update

--- src/bbs/rpgmanager/Manager.java
+++ src/bbs/rpgmanager/Manager.java
@@ -1,9 +1,18 @@
 package bbs.rpgmanager;
 
+import java.sql.Date;
+
+import bbs.rpgmanager.db.RpgmJdbcTemplate;
+
 public class Manager {
 
-	public static void main(String[] args) {
-System.out.println("Hello World");
-	}
+	public static void main(String[] args)  {
+		PlayerManager pm = new PlayerManager(RpgmJdbcTemplate.getTemplate());
+		System.out.println(pm.getPlayer(2));
+		pm.delPlayer(3);
+		pm.addPlayer("Frederick", "Warnant", "Squatman",new Date(1976-9-11), "warnant.frederick@gmail.com", 472484322, "Court-Saint-Etienne", "1490", "Rue du Bois des Rêves", 6, "", "Grave mais efficace\nMinimaxer");
+		System.out.println(pm.getPlayer(3));
 
+	
+	}
 }
